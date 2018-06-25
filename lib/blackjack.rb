dee76d73c43baffae10034960abcc8b7c87e2033
Christopher Arnold <chrisarnold2010@gmail.com>
2018-06-25 17:14:29 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -34,13 +34,15 @@ def hit?(card_total)
   if get_user_input = 'h'
     deal_card
     card_total += deal_card
-  else 
+  elsif get_user_input != 's' || get_user_input != 'h' 
     invalid_command
+  else
+    prompt_user
   end
 end
 
 def invalid_command
-  # code invalid_command here
+  puts "Please enter a valid command"
 end
 
 #####################################################
